68ef3e01813e929bb396a7f1edf77a87297e5322
Unknown <luis.carmonacastro@ucr.ac.cr>
2018-05-22 17:10:34 -0600

    Algoritmos

--- AlgoritmosIndexada.cpp
+++ AlgoritmosIndexada.cpp
@@ -15,24 +15,67 @@ void AlgoritmosIndexada::listar(lista l){
     }
 }
 
-bool AlgoritmosIndexada::simetrica(lista){
-
+bool AlgoritmosIndexada::simetrica(lista l){
+    int derecha = l.numElem();
+    int izquierda = 1;
+    bool seguir = true;
+    while(seguir&&(izquierda<=derecha)){
+        if(l.recuperar(izquierda)==l.recuperar(derecha)){
+            derecha--;
+            izquierda++;
+        }else{
+            seguir = false;
+        }
+    }
+    return seguir;
 }
 
-void AlgoritmosIndexada::invertir(lista){
-
+void AlgoritmosIndexada::invertir(lista l){
+    int derecha = l.numElem();
+    int izquierda = 1;
+    bool seguir = true;
+    while(izquierda<=derecha){
+        l.intercambiar(izquierda,derecha);
+        derecha--;
+        izquierda++;
+    }
 }
 
-bool AlgoritmosIndexada::buscar(int, lista){
-
+bool AlgoritmosIndexada::buscar(int indice, lista l){
+    int indiceRecorrido = 1;
+    int max = l.numElem();
+    bool seguir = true;
+    bool encontrado = false;
+    while((indiceRecorrido<=max)&&seguir){
+        if(l.recuperar(indiceRecorrido)==l.recuperar(indice)){
+            seguir = false;
+            encontrado = true;
+        }
+        indiceRecorrido++;
+    }
+    return encontrado;
 }
 
-void AlgoritmosIndexada::eliminarRepetidos(lista){
-
+void AlgoritmosIndexada::eliminarRepetidos(lista l){
+    int indice = 1;
+    while(indice < l.numElem()){
+        if(l.recuperar(indice)==l.recuperar(indice+1)){
+            l.borrar(indice+1);
+        }else{
+            indice++;
+        }
+    }
 }
 
-bool AlgoritmosIndexada::subLista(lista,lista){
-
+bool AlgoritmosIndexada::subLista(lista l1,lista l2){
+    int indiceL1 = 1;
+    int indiceL2 = 1;
+    int max = l2.numElem();
+    while(indiceL2<=max){
+        while(l1.recuperar(indiceL1)==l2.recuperar(indiceL2)){
+            
+        }
+    }
 }
 
 bool AlgoritmosIndexada::iguales(lista,lista){
